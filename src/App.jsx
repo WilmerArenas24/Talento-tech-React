ff9d67ef2791b294114bba5c7578450a0121f403
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-11 20:15:50 -0500

    creando algunas funciones de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-
+import { useState } from 'react';
 import './App.css'
+
+function Profile(){
+  const user = {
+    name: 'Elvis',
+    imageUrl:'https://hips.hearstapps.com/hmg-prod/images/singer-elvis-presley-news-photo-1590531497.jpg',
+    imageSize:90,
+  };
+
+  return (
+    <>
+    <h1>{user.name}</h1>
+    <br />
+    <img 
+    className='avatar'
+    src={user.imageUrl}
+    alt={'photo of '+user.name}
+    style={{
+      width: user.imageSize,
+      height: user.imageSize
+    }}
+
+     />
+    </>
+  )
+}
 
 function App() {
 
@@ -11,13 +36,24 @@ function App() {
     console.log(e.target.value)
   }
 
+  // Cambiando el estado de las variables
+const [count, setCount] = useState(0);
 
 
   return (
         <>
-          <h1>Hola mundo</h1>
+          <Profile/>
+          <br />
+          
           <button onClick={() => saludar()}>Enviar</button>
           <input type="text" onChange={mostrarTexto} />
+          <br />
+          
+          <button onClick={() => setCount(count+1)}>Sumar</button>
+          <button onClick={() => setCount(count-1)}>Restar</button>
+          <p>El contador va en: {count}</p>
+          
+
         </>
   )
 }
