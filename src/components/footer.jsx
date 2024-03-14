0f4ba6c6ae4c31c78a2a575cbc14f4a53e60c8f5
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-13 19:29:09 -0500

    Implementando estilos a footer con taildwind

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,11 @@
 
 export default function Footer(){
     return(
-        <div>
-            <h1>Footer</h1>
+        <footer className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto">
+          <p className="text-center">&copy; {new Date().getFullYear()} Proyecto TalentoTech</p>
+          <p className="text-center">Desarrollado por Wilmer Arenas</p>
         </div>
+      </footer>
     );
 }
