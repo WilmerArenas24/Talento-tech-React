0c1f40230d8e4ffa7b2ae4f5ef2da0d92da9c5bc
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-15 18:56:13 -0500

    creando el slice de redux

---
+++ src/features/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const users = [
+    {
+        "id": 1145,
+        "name": "JHON",
+        "lastname": "DOE",
+        "email": "johndoe11@example.com",
+        "password": "password1"
+      }
+    
+]
+
+export const userSlice = createSlice({
+    name: 'users',
+    initialState: users,
+    reducers: {
+        addUser: (state, action) =>{
+            state.push(action.payload)
+        }
+
+    }
+
+})
+
+export const {addUser} = userSlice.actions
+export default userSlice.reducer
