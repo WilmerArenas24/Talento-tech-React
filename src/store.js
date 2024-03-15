4dc8b25b954a760cdbd348fd12d7a419d65f4177
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-14 20:04:50 -0500

    Agregando slice a store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import numberReducer from './features/numberSlice'
 
 // Agrupamos los estados en una sola ubicacion
 const store  = configureStore({
-    reducer:{}
+    reducer:{
+        // agregando slice
+        number: numberReducer
+
+    }
 })
 
 export default store;
