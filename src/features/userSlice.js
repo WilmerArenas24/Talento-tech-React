f192ef77759426fd9462cae1090496fd0a7b80a5
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-18 19:01:12 -0500

    Definiendo una Api con redux

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,25 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const users = [
-    {
-        "id": 1145,
-        "name": "JHON",
-        "lastname": "DOE",
-        "email": "johndoe11@example.com",
-        "avatar": "hdhdhdhdhdhshshahaggagaag"
-      }
+export const userSlice = createApi({
+    reducerPath: 'users',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'localhost:3000' // Corregir la URL base
+        // baseUrl: 'http://localhost:3000' // Corregir la URL base
+    }),
+    endpoints: builder => ({
+        getUsers: builder.query({
+            query: () => '/user'
+        })
+    })
+});
+
+export const { usegetUsersQuery } = userSlice;
+
+
+// const users = [
+//     {
+//         "id": 1145,
+//         "name": "JHON",
+//         "lastname": "DOE",
+//         "email": "johndoe11@example.com",
+//         "avatar": "hdhdhdhdhdhshshahaggagaag"
+//       }
     
-]
+// ]
+//metodo para usar el createslice de reduxtookit
+// export const userSlice = createSlice({
+//     name: 'users',
+//     initialState: users,
+//     reducers: {
+//         addUser: (state, action) =>{
+//             state.push(action.payload)
+//         }
+//     }
+// })
 
-export const userSlice = createSlice({
-    name: 'users',
-    initialState: users,
-    reducers: {
-        addUser: (state, action) =>{
-            state.push(action.payload)
-        }
-    }
-})
-
-export const {addUser} = userSlice.actions
-export default userSlice.reducer
+// export const {addUser} = userSlice.actions
+// export default userSlice.reducer
