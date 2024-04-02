b33d5cb40389e1fc87d9832259e36cc8a3324df0
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-04-01 20:26:24 -0500

    Corrigiendo estilos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,7 @@ export default function Footer(){
 
     return (
         <div className="bg-gray-800 text-white py-4 text-center  bottom-0 w-full">
-            <h1>Proyecto talento Tech</h1>
-            <h2 className="text-red-500">Desarrollador por Juanito</h2>
+            &copy; Talento tech - Desarrollado por Wilmer Arenas
         </div>
     );
 }
